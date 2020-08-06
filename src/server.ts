fa675be1865ea48506eee59a6d259fde8b1a88b5
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-08-06 11:24:12 -0300

    Configuring structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { request, response } from 'express';
+
+const app = express();
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({message: 'Hello World'})
+});
+
+
+app.listen(3333, () => {
+  console.log('Server is running!!');
+});
