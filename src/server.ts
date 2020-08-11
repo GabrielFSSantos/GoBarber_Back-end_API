723b46e32b3ad6137629a13d9a14f41cd567a771
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-08-11 10:37:26 -0300

    Registration of Appointments

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'})
-});
-
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server is running!!');
