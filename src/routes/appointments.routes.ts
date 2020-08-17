ce835346aa49392eafaeb2900bd32e4b31be8468
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-08-17 15:12:53 -0300

    Creating records

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -18,16 +18,16 @@ appointmentRouter.get('/', async (request, response) => {
 
 appointmentRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parseDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
-  
-    const appointment = await createAppointment.execute({provider, date: parseDate});
-  
+
+    const appointment = await createAppointment.execute({ provider_id, date: parseDate });
+
     return response.json(appointment);
-    
+
   } catch (err) {
     return response.status(400).json({ error: err.messege });
   }
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -18,12 +18,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository'
 import appointmentRouter from '../routes/appointments.routes';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
 
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
@@ -36,7 +36,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
