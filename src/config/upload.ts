7290d08752081db740f0a630dc7fb5eebada6b01
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-08-20 11:27:39 -0300

    Updating avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,10 +2,14 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tpm');
+
 export default {
+  directory: tmpFolder,
+
   storage: multer.diskStorage({
 
-    destination: path.resolve(__dirname, '..', '..', 'tpm'),
+    destination: tmpFolder,
 
     filename(request, file, callback) {
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,11 @@
-import { Router, request, response } from 'express';
-import CreateUserService from '../services/CreateUserService';
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
+
+import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -20,15 +23,28 @@ usersRouter.post('/', async (request, response) => {
     });
 
     delete user.password;
-    
-    return response.json(user); 
+
+    return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  return response.json({ ok: true });
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 })
 
 export default usersRouter;
