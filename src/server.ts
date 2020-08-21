ebf9a6a4afaff35bd9223041b7295c4f3e032e63
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-08-21 11:19:17 -0300

    Dealing with errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata';
-import express, {Request, Response, NextFunction} from 'express';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
-import './database/index';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+
+import './database/index';
 
 const app = express();
 app.use(express.json());
@@ -11,8 +15,20 @@ app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
 
-})
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server is running!!');
